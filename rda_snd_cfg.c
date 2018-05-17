69d4592fd9854bec4dbce71de091b752231b686f
dmitriy <ddkonev@gmail.com>
2018-05-17 22:14:37 +0300

    minor changes

--- rda_snd_cfg.c
+++ rda_snd_cfg.c
@@ -119,13 +119,13 @@ void close_mixer(void)
 
 void main(int argc, char *argv[])
 {
-	int channels = 2, ret, g_codec_start;
-	unsigned int sample_rate = 44100;
+	// see all defines in kernel - rda_codec_adp.h
 
-	// 3. open mixer
+	int ret;
+
 	if (open_mixer()) {
 		printf("open mixer err, exit. \n");
-		goto mixer_err;
+		return;
 	}
 
 	//*************
@@ -185,112 +185,6 @@ void main(int argc, char *argv[])
 
 	printf("%s:%s[%d] ret = %d\n", __FILE__, __func__, __LINE__, ret);
 
-
-
-	printf("hello world\n");
-	//close_mixer();
-
-mixer_err:
-	printf("err\n");
+	close_mixer();
 }
 
-#if 0
-[   27.961914] rda_gouda_enable_store in
-[   28.198181] rda calibdata: GET_MIC_GAINS - ana - 18, adc - 0, alg - 6.
-[   28.198852] rda calibdata: GET_MIC_GAINS - ana - 12, adc - 0, alg - 3.
-[   28.199645] rda calibdata: GET_MIC_GAINS - ana - 9, adc - 0, alg - 3.
-[   28.215637] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: ITF, val: 2
-[   28.216613] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: SpkSel, val: 2
-[   28.217712] audio SPK = 2
-[   28.217712] 
-[   28.218200] audio gpio extpa is invalid !
-[   28.218200] 
-[   28.218872] audio gpio extpa 1 is invalid !
-[   28.218872] 
-[   28.219970] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   28.220458] <rda_msys>: end of pending msg
-[   28.225646] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: Capture Volume, val: 2
-[   28.226684] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: ForceMainMic, val: 0
-[   28.228332] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   28.228820] <rda_msys>: end of pending msg
-[   28.236755] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: Commit Setup, val: 0
-[   28.238220] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   28.238708] <rda_msys>: end of pending msg
-[   28.250793] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: OutSampleRate, val: 44100
-[   28.251892] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: OutChannelNumber, val: 2
-[   28.253112] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: InSampleRate, val: 8000
-[   28.254333] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: InChannelNumber, val: 1
-[   28.367736] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: Playback Volume, val: 7
-[   28.368774] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: Commit Setup, val: 0
-[   28.370422] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   28.370910] <rda_msys>: end of pending msg
-[   29.486877] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: ITF, val: 1
-[   29.487854] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: SpkSel, val: 1
-[   29.488952] audio SPK = 1
-[   29.488952] 
-[   29.490539] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   29.491088] <rda_msys>: end of pending msg
-[   29.491638] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: Capture Volume, val: 2
-[   29.492797] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: ForceMainMic, val: 1
-[   29.494567] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   29.495117] <rda_msys>: end of pending msg
-[   29.495727] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: Commit Setup, val: 0
-[   29.498107] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   29.498596] <rda_msys>: end of pending msg
-[   29.506530] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: OutSampleRate, val: 44100
-[   29.507690] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: OutChannelNumber, val: 2
-[   29.509216] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: CodecAppMode, val: 0
-[   29.510559] sound/soc/rda/rda_codec.c:rda_codec_codec_app_mode[339] condec: rda-codec
-[   29.511718] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   29.512268] <rda_msys>: end of pending msg
-[   29.513122] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: StartPlay, val: 0
-[   29.515930] --->sound/soc/rda/rda_codec.c:aud_StreamStart[99] itf: 1, codec: rda-codec, stream: ce30bbb0, cfg: ce30bbcc
-[   29.517517] --->arch/arm/plat-rda/md_sys.c:rda_msys_send_cmd[326]
-[   29.519287] --->arch/arm/plat-rda/md_sys.c:rda_msys_send_cmd[344]
-[   29.576232] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   29.576721] <rda_msys>: end of pending msg
-[   29.577270] --->sound/soc/rda/rda_codec.c:aud_StreamStart[106]
-[   29.578063] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: ITF, val: 1
-[   29.579040] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: SpkSel, val: 1
-[   29.580139] audio SPK = 1
-[   29.580139] 
-[   29.581054] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   29.581542] <rda_msys>: end of pending msg
-[   29.582336] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: Capture Volume, val: 2
-[   29.583496] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: ForceMainMic, val: 1
-[   29.586791] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   29.587341] <rda_msys>: end of pending msg
-[   29.588195] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: Commit Setup, val: 0
-[   29.589721] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   29.590270] <rda_msys>: end of pending msg
-[   29.593688] audio [start aud ifc] : substream->stream is [0], PLAYBACK, cmd is [1] 
-[   29.593688] 
-[   31.015380] rda_gouda_stretch_pre_wait_and_enable_clk : gouda is active.
-[   31.965332] rda_gouda_stretch_pre_wait_and_enable_clk : gouda is active.
-[   32.465332] rda_gouda_stretch_pre_wait_and_enable_clk : gouda is active.
-[   33.315368] rda_gouda_stretch_pre_wait_and_enable_clk : gouda is active.
-[   33.825317] rda_gouda_stretch_pre_wait_and_enable_clk : gouda is active.
-[   34.078369] audio [stop aud ifc] : substream->stream is [0], PLAYBACK, cmd is [0] 
-[   34.078369] 
-[   34.079650] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: Stop, val: 0
-[   34.092407] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   34.092407] <rda_msys>: end of pending msg
-[   34.269836] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: ITF, val: 2
-[   34.269897] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: SpkSel, val: 2
-[   34.269897] audio SPK = 2
-[   34.269897] 
-[   34.269897] audio gpio extpa is invalid !
-[   34.269897] 
-[   34.269958] audio gpio extpa 1 is invalid !
-[   34.269958] 
-[   34.270385] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   34.270385] <rda_msys>: end of pending msg
-[   34.270568] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: Capture Volume, val: 2
-[   34.270629] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: ForceMainMic, val: 0
-[   34.271057] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   34.271057] <rda_msys>: end of pending msg
-[   34.271423] -->sound/core/control.c:snd_ctl_elem_write[915] codec: rda-codec, ctrl: Commit Setup, val: 0
-[   34.271850] <rda_msys>: pid = 39, rda-codec: ext_len = 0
-[   34.271850] <rda_msys>: end of pending msg
-[   34.655334] rda_gouda_stretch_pre_wait_and_enable_clk : gouda is active.
-#endif
